9f0a989d7cf64d8a2da72655d914e6bea9328663
leangsengLy <edeepleang@gmail.com>
2025-12-03 15:26:47 +0700

    add new yaml

--- project_api_car/project_api_car/src/main/java/com/example/project_api_car/ProjectApiCarApplication.java
+++ project_api_car/project_api_car/src/main/java/com/example/project_api_car/ProjectApiCarApplication.java
@@ -1,13 +1,13 @@
-package carAuto.carage.project_web_car;
+package com.example.project_api_car;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ProjectWebCarApplication {
+public class ProjectApiCarApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(ProjectWebCarApplication.class, args);
+		SpringApplication.run(ProjectApiCarApplication.class, args);
 	}
 
 }
--- project_api_car/project_api_car/src/test/java/com/example/project_api_car/ProjectApiCarApplicationTests.java
+++ project_api_car/project_api_car/src/test/java/com/example/project_api_car/ProjectApiCarApplicationTests.java
@@ -1,10 +1,10 @@
-package carAuto.carage.project_web_car;
+package com.example.project_api_car;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class ProjectWebCarApplicationTests {
+class ProjectApiCarApplicationTests {
 
 	@Test
 	void contextLoads() {
